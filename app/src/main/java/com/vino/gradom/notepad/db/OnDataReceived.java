9981a46c87e846c8aeef813d052999628418b953
Tejaskumar264 <tejas7061506510@gmail.com>
2021-12-14 15:38:52 +0530

    Notepad-App

--- app/src/main/java/com/vino/gradom/notepad/db/OnDataReceived.java
+++
@@ -1,9 +0,0 @@
-package com.vino.gradom.notepad.db;
-
-import com.vino.gradom.notepad.model.Note;
-
-import java.util.LinkedList;
-
-public interface OnDataReceived {
-    void onReceived(LinkedList<Note> noteList);
-}
--- app/src/main/java/com/vino/gradom/notepad/db/OnNoteSaved.java
+++
@@ -1,5 +0,0 @@
-package com.vino.gradom.notepad.db;
-
-public interface OnNoteSaved {
-    void onSaved(int msgResId);
-}
